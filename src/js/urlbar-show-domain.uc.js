7bcef3a84e34eeed46caba3f7e0f085cda5ccbd1
Vu Nguyen <vund.vn@gmail.com>
2023-05-29 19:25:20 +0700

    Add first userscript: urlbar-show-domain

---
+++ src/js/urlbar-show-domain.uc.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name            URL bar show domain
+// @author          vufly
+// @description     Show only domain in the URL bar input field
+// @version         2023-05-29 02:30  Initial
+// ==/UserScript==
+(function () {
+  // wait for delayed startup for some parts of the script to execute.
+  if (gBrowserInit.delayedStartupFinished) {
+    init();
+  } else {
+    let delayedListener = (subject, topic) => {
+      if (topic == "browser-delayed-startup-finished" && subject == window) {
+        Services.obs.removeObserver(delayedListener, topic);
+        init();
+      }
+    };
+    Services.obs.addObserver(
+      delayedListener,
+      "browser-delayed-startup-finished"
+    );
+  }
+
+  function init() {
+    const originalSetValue = UrlbarInput.prototype._setValue,
+          urlbarValueFormater = new UrlbarValueFormatter(gURLBar);
+
+    UrlbarInput.prototype._setValue = function (val, allowTrim) {
+      originalSetValue.call(this, val, allowTrim);
+      if (val)
+        replaceUrlByDomain();
+    }
+    gURLBar.inputField.addEventListener('blur', replaceUrlByDomain);
+
+    function replaceUrlByDomain() {
+      const urlMetadata = urlbarValueFormater._getUrlMetaData();
+      if (urlMetadata) {
+        const domain = urlMetadata.domain;
+        gURLBar.inputField.value = domain;
+      }
+    }
+  }
+})();
---
+++ src/scripts/buildJs.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+
+const pathToSubs = '../submodules/';
+const pathToMine = '../js/';
+
+const theirFiles = [].map(file => path.join(__dirname, pathToSubs + file));
+
+const myFiles = [
+  'urlbar-show-domain.uc.js'
+].map(file => path.join(__dirname, pathToMine + file));
+
+const destinationDirectory = './profile/chrome/JS';
+
+// Create the destination directory if it doesn't exist
+if (!fs.existsSync(destinationDirectory)) {
+  fs.mkdirSync(destinationDirectory, { recursive: true });
+}
+
+// Iterate over the list of files
+[...theirFiles, ...myFiles].forEach(source => {
+  const fileName = path.basename(source);
+  const destination = path.join(destinationDirectory, fileName);
+
+  // Create a readable stream from the source file
+  const sourceStream = fs.createReadStream(source);
+
+  // Create a writable stream to the destination file
+  const destinationStream = fs.createWriteStream(destination);
+
+  // Copy the file by piping the source stream to the destination stream
+  sourceStream.pipe(destinationStream);
+
+  // Handle the 'finish' event to know when the file copy is complete
+  destinationStream.on('finish', () => {
+    console.log(`File ${source} copied to ${destination} successfully.`);
+  });
+
+  // Handle any error that occurs during the copying process
+  destinationStream.on('error', (err) => {
+    console.error(`Error occurred while copying ${source}:`, err);
+  });
+});
