2d9ae45a9c4b802e3b828056195374bc77e76344
Nguyen Dinh Vu <nguyendinhvu@msn.com>
2024-02-26 17:59:04 +0700

    sidebar-one: adjust order of sidebar-header element

--- src/js/sidebar-one.uc.js
+++ src/js/sidebar-one.uc.js
@@ -37,8 +37,13 @@
       padding: 2px;
     }
 
-    #sidebar-box #sidebar-collapse {
+    :root[foxinity] #sidebar-box:not([positionend="true"]) #sidebar-header {
+      flex-direction: row-reverse;
+    }
+
+    #sidebar-collapse {
       margin: 0;
+      padding: 8px 10px;
     }
 
     #sidebar-box #sidebar-collapse > image {
@@ -49,7 +54,7 @@
       list-style-image: url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12.08,4.08L20,12L12.08,19.92L10.67,18.5L16.17,13H2V11H16.17L10.67,5.5L12.08,4.08M20,12V22H22V2H20V12Z" fill="context-fill" fill-opacity="context-fill-opacity"/></svg>');
     }
 
-    #sidebar-box #sidebar-collapse > .toolbarbutton-text {
+    #sidebar-collapse > .toolbarbutton-text {
       display: none;
     }
 
