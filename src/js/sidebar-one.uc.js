b1ff251668d6be60f17ce7be24e2d1391cff9789
Nguyen Dinh Vu <nguyendinhvu@msn.com>
2024-02-27 15:39:15 +0700

    sidebar-one: add sidebar-settings button

--- src/js/sidebar-one.uc.js
+++ src/js/sidebar-one.uc.js
@@ -28,22 +28,64 @@
 
   function init() {
     const css = `
+    :root[foxinity] #sidebar-throbber,
+    :root[foxinity] #sidebar-spacer,
+    :root[foxinity] #sidebar-close {
+      display: none;
+    }
+
+    :root[foxinity] #sidebar-switcher-target {
+      flex: 0;
+    }
+
     :root[foxinity] #sidebar-box {
       max-width: 99vw;
     }
 
     :root[foxinity] #sidebar-header {
+      justify-content: space-between;
       background: var(--toolbar-bgcolor);
-      padding: 2px;
+      padding: 2px 4px;
+    }
+
+    #sidebarMenu-popup #sidebar-switcher-bookmarks {
+      --webextension-menuitem-image: url(chrome://browser/skin/bookmark.svg);
+      --webextension-menuitem-image-2x: url(chrome://browser/skin/bookmark.svg);
+    }
+
+    #sidebarMenu-popup #sidebar-switcher-history {
+      --webextension-menuitem-image: url(chrome://browser/skin/history.svg);
+      --webextension-menuitem-image-2x: url(chrome://browser/skin/history.svg);
+    }
+
+    #sidebarMenu-popup #sidebar-switcher-tabs {
+      --webextension-menuitem-image: url(chrome://browser/skin/synced-tabs.svg);
+      --webextension-menuitem-image-2x: url(chrome://browser/skin/synced-tabs.svg);
+    }
+
+    #sidebarMenu-popup #sidebar-reverse-position {
+      --webextension-menuitem-image: url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="2 3 20 18"><path d="M8,10V13H14V18H8V21L2,15.5L8,10M22,8.5L16,3V6H10V11H16V14L22,8.5Z" fill="context-fill" fill-opacity="context-fill-opacity"/></svg>');
+      --webextension-menuitem-image-2x: url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="2 3 20 18"><path d="M8,10V13H14V18H8V21L2,15.5L8,10M22,8.5L16,3V6H10V11H16V14L22,8.5Z" fill="context-fill" fill-opacity="context-fill-opacity"/></svg>');
+    }
+
+    #sidebarMenu-popup [data-l10n-id="sidebar-menu-close"] {
+      --webextension-menuitem-image: url(chrome://global/skin/icons/close.svg);
+      --webextension-menuitem-image-2x: url(chrome://global/skin/icons/close.svg);
+    }
+
+    :root[foxinity] #sidebarMenu-popup image {
+      -moz-context-properties: fill;
+      fill: currentColor;
     }
 
     :root[foxinity] #sidebar-box:not([positionend="true"]) #sidebar-header {
       flex-direction: row-reverse;
     }
 
-    #sidebar-collapse {
+    #sidebar-collapse,
+    #sidebar-settings {
       margin: 0;
-      padding: 8px 10px;
+      padding: 6px 6px;
     }
 
     #sidebar-box #sidebar-collapse > image {
@@ -54,7 +96,12 @@
       list-style-image: url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12.08,4.08L20,12L12.08,19.92L10.67,18.5L16.17,13H2V11H16.17L10.67,5.5L12.08,4.08M20,12V22H22V2H20V12Z" fill="context-fill" fill-opacity="context-fill-opacity"/></svg>');
     }
 
-    #sidebar-collapse > .toolbarbutton-text {
+    #sidebar-settings > image {
+      list-style-image: url(chrome://global/skin/icons/settings.svg);
+    }
+
+    #sidebar-collapse > .toolbarbutton-text,
+    #sidebar-settings > .toolbarbutton-text {
       display: none;
     }
 
@@ -68,7 +115,7 @@
 
     SidebarUI._header = document.getElementById('sidebar-header');
 
-    //Create collapse toolbarbutton
+    // Create sidebar collapse toolbarbutton
     SidebarUI._collapseButton = dom.toolbarbutton(document, false, {
       id: 'sidebar-collapse',
       label: 'Collapse',
@@ -77,7 +124,6 @@
       class: 'subviewbutton subviewbutton-iconic',
       key: 'sidebarCollapseBtn'
     });
-  
     SidebarUI._header.insertBefore(SidebarUI._collapseButton, SidebarUI._header.firstChild);
   
     SidebarUI.toggleCollapse = function () {
@@ -86,6 +132,17 @@
       SidebarUI._box.classList.add('animating');
       setTimeout(() => SidebarUI._box.classList.remove('animating'), 200);
     }
+
+    // Create sidebar settings toolbarbutton
+    SidebarUI._settingsButton = dom.toolbarbutton(document, false, {
+      id: 'sidebar-settings',
+      label: 'Settings',
+      tooltiptext: 'Settings',
+      oncommand: 'console.log("Sidebar settings");',
+      class: 'subviewbutton subviewbutton-iconic',
+      key: 'sidebarSettingsBtn'
+    });
+    SidebarUI._header.append(SidebarUI._settingsButton);
   }
 
   var dom = (_ => {
