aee6f70ede491931e896f83e4f887a64ab6f97d2
Nguyen Dinh Vu <nguyendinhvu@msn.com>
2024-08-25 02:15:08 +0700

    urlbar-show-domain: Fix the URL input value didn't recover on focusing

--- src/js/urlbar-show-domain.uc.js
+++ src/js/urlbar-show-domain.uc.js
@@ -2,6 +2,7 @@
 // @name            URL bar show domain
 // @author          vufly
 // @description     Show only domain in the URL bar input field
+// @version         2024-08-25 02:13  Fix the URL input value didn't recover on focusing
 // @version         2024-01-13 01:40  Delay the domain trimming after the valueFormatter.update() because it's async
 // @version         2023-05-31 23:00  Strip out www.
 // @version         2023-05-29 02:30  Initial
@@ -24,18 +25,21 @@
   }
 
   function init() {
-    const originalSetValue = UrlbarInput.prototype._setValue,
-          urlbarValueFormater = new UrlbarValueFormatter(gURLBar);
+    var originalSetValue = UrlbarInput.prototype._setValue,
+      urlbarValueFormater = new UrlbarValueFormatter(gURLBar);
 
-    UrlbarInput.prototype._setValue = function (val, allowTrim) {
-      originalSetValue.call(this, val, allowTrim);
+    UrlbarInput.prototype._setValue = function (val, ...args) {
+      originalSetValue.call(this, val, ...args);
       if (val)
         this.window.requestAnimationFrame(_ => replaceUrlByDomain());
     }
     gURLBar.inputField.addEventListener('blur', replaceUrlByDomain);
+    gURLBar.inputField.addEventListener('focus', _ => {
+      gURLBar.inputField.value = gURLBar.untrimmedValue;
+    })
 
     function replaceUrlByDomain() {
-      const urlMetadata = urlbarValueFormater._getUrlMetaData();
+      var urlMetadata = urlbarValueFormater._getUrlMetaData();
       if (urlMetadata) {
         let domain = urlMetadata.domain;
         if (domain.startsWith("www.")) {
